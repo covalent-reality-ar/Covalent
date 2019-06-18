272d87fab72e06a6f5297275060eaac3cc8f3ffc
John Loomis <johnaloomis@outlook.com>
2019-06-18 15:46:06 -0400

    Covalent from AR template

--- js/click.js
+++ js/click.js
@@ -1,13 +1,1 @@
 
-AFRAME.registerComponent('marker', {
-    init: function () {
-        const redirect = document.getElementById("Logo");
-
-
-
-        redirect.addEventListener('click', function (evt) {
-            window.location.replace("https://covalentreality.co/");
-        })
-
-    }
-});
